0f26dfff0b1535e142f2ec494d0dbd81a24ac42a
Gabriel Pezzo <85529577+Pezzo70@users.noreply.github.com>
2022-04-18 10:14:01 -0300

    Ideia inicial da execução do exercicio

--- src/War/GamePlay/Battlefield.php
+++ src/War/GamePlay/Battlefield.php
@@ -7,5 +7,39 @@ use Galoa\ExerciciosPhp2022\War\GamePlay\Country\CountryInterface;
 /**
  * A manager that will roll the dice and compute the winners of a battle.
  */
+
+
+
+
 class Battlefield implements BattlefieldInterface {
+   
+    public function rollDice(CountryInterface $country, bool $isAttacking): array{
+        $diceValues = array();
+        $isAttacking ? $troops = $country -> getNumberOfTroops() - 1 : $troops = $country -> getNumberOfTroops();
+        for($i = 0; $i <= $troops; $i++){
+              $diceValues[] = rand()&6;
+        }
+        sort($diceValues);
+        return $diceValues[];
+    }
+    
+    public function computeBattle(CountryInterface $attackingCountry, array $attackingDice, CountryInterface $defendingCountry, array $defendingDice): void{
+        sizeof($attackingDice) >= sizeof($defendingDice) ? $isAttackingTroopsBiggerOrEqual = true : $isAttackingTroopsBiggerOrEqual = false;
+        if($isAttackingTroopsBiggerOrEqual){
+            foreach ($attackingDice as $key => $value) {
+                if($attackingDice[$key] > $defendingDice[$key]){
+                   $defendingCountry -> killTroops(1);
+                }else{
+                    $attackingCountry -> killTroops(1);
+                }
+            }
+        }else{
+            foreach ($defendingDice as $key => $value) {
+                if($attackingDice[$key] > $defendingDice[$key]){
+                   $defendingCountry -> killTroops(1);
+                }else{
+                    $attackingCountry -> killTroops(1);
+                }
+        }
+    }
 }
